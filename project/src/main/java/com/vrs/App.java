b58e01e7190e49c93eb819c949a4bab6c1637d0f
Keegan J <keeganjohnson02@gmail.com>
2023-03-07 16:15:24 -0500

    project template

---
+++ project/src/main/java/com/vrs/App.java
@@ -0,0 +1,13 @@
+package com.vrs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
