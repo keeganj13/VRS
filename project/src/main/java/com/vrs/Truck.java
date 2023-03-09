899094cac53c79fbb08d7af190b9fbbe7db2894e
Darrell Martin <darrellejm29@comcast.net>
2023-03-09 13:33:44 -0500

    Motorcycle & Truck Classes

---
+++ project/src/main/java/com/vrs/Truck.java
@@ -0,0 +1,11 @@
+public class Truck
+{
+	public int make;
+	public int model;
+	public int year;
+    public int mpg;
+    public int color;
+    public String engineType;
+    public int type;
+    public double load;
+}
