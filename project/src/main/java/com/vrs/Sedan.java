0b2012ef29d66ce8d35eecde379ac08c25ba61d7
Sam <samuelrmt@hotmai.com>
2023-03-23 14:00:44 -0400

    did things that helped

--- project/src/main/java/com/vrs/Sedan.java
+++ project/src/main/java/com/vrs/Sedan.java
@@ -2,4 +2,12 @@ package com.vrs;
 
 public class Sedan extends Vehicle {
     public int numSeats;
+    public Sedan (String makeS, String modelS, int yearS, String colorS, String engineTypeS, int numSetasS)
+    {
+           this.make = makeS;
+           this.model = modelS;
+           this.year = yearS;
+           this.color = colorS;
+           this.engineType = engineTypeS;
+   }
 }
