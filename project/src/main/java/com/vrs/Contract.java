166b34553f6812b65b8c1b075a35b11c8d6e7935
Keegan Johnson <keeganjohnson02@gmail.com>
2023-03-23 13:27:22 -0400

    contract update

--- project/src/main/java/com/vrs/Contract.java
+++ project/src/main/java/com/vrs/Contract.java
@@ -2,4 +2,8 @@ package com.vrs;
 
 public class Contract {
     public Customer customer;
+    public Date rentDate;
+    public Vehicle[] vehicleList;
+    int rentPeriod;
+    double totalPrice;
 }
