8e1166730ddb4491e27c1c679a8f880aaf877a80
Sam <samuelrmt@hotmai.com>
2023-05-04 13:23:00 -0400

    //created the suv class

--- project/src/main/java/com/vrs/SUV.java
+++ project/src/main/java/com/vrs/SUV.java
@@ -1,5 +1,14 @@
 package com.vrs;
 
 public class SUV extends Vehicle {
-    
+    public int numSeats;
+    public SUV (String makeSUV, String modelSUV, int yearSUV, String colorSUV, String engineTypeSUV, int numSetasSUV)
+    {
+           this.make = makeSUV;
+           this.model = modelSUV;
+           this.year = yearSUV;
+           this.color = colorSUV;
+           this.engineType = engineTypeSUV;
+   }
 }
+
