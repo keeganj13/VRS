58de2487f7239d492bcfb139e749b910154f606f
HerobaneNair <srirambnair@gmail.com>
2023-05-04 13:13:05 -0400

    Fixing bugs in sedan class
    Fixes bad naming conventions and added numSeats to constructor

--- project/src/main/java/com/vrs/Sedan.java
+++ project/src/main/java/com/vrs/Sedan.java
@@ -2,12 +2,13 @@ package com.vrs;
 
 public class Sedan extends Vehicle {
     public int numSeats;
-    public Sedan (String makeS, String modelS, int yearS, String colorS, String engineTypeS, int numSetasS)
+    public Sedan (String make, String model, int year, String color, String engineType, int numSeats)
     {
-           this.make = makeS;
-           this.model = modelS;
-           this.year = yearS;
-           this.color = colorS;
-           this.engineType = engineTypeS;
+           this.make = make;
+           this.model = model;
+           this.year = year;
+           this.color = color;
+           this.engineType = engineType;
+           this.numSeats = numSeats;
    }
 }
