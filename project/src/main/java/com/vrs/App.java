36ec27592ca8caf53f99c5cd0ba545121ada38ec
HerobaneNair <srirambnair@gmail.com>
2023-05-08 06:18:49 -0400

    Update App.java

--- project/src/main/java/com/vrs/App.java
+++ project/src/main/java/com/vrs/App.java
@@ -1,16 +1,7 @@
 package com.vrs;
-
-import javax.swing.JFrame;
-
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
-        LoginFrame LF = new LoginFrame();
-        LF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        LF.setSize(400, 300);
-        LF.setVisible(true);
+        GUI window = new GUI();
+        window.setVisible(true);
     }
 }
