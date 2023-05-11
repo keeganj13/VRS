4efeb27f9ee54328acd1c482377abd5d59f3e069
HerobaneNair <srirambnair@gmail.com>
2023-05-11 12:14:05 -0400

    Update Motorcycle.java

--- project/src/main/java/com/vrs/Motorcycle.java
+++ project/src/main/java/com/vrs/Motorcycle.java
@@ -1,18 +1,84 @@
 package com.vrs;
-
+import java.io.*;
+import java.util.ArrayList;
 public class Motorcycle extends Vehicle {
-
-    public Motorcycle(String make, String model, double mpg, int year, String color, String engine) {
-        super(make, model, mpg, year, color, engine);
+    public Motorcycle(String make, String model, int year, double mpg, String color, String engine) {
+        super(make, model, year, mpg, color, engine);
+    }
+    public void honk() {
+        System.out.println("Beep beep!");
     }
 
-    public Motorcycle() {
-        make = "Yamaha";
-        model = "YZF-R1M";
-        MPG = 33;
-        year = 2023;
-        color = "Black";
-        engineType = "gas";
+    public static String[] read() {
+        isFile();
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            // Create a new FileReader object for the file
+            FileReader reader = new FileReader("Motorcycle.txt");
+
+            // Create a new BufferedReader object to read from the FileReader
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            // Read each line of text from the file and add it to the ArrayList
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Close the BufferedReader object to release any resources it was using
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // Convert the ArrayList to a String array and return it
+        String[] linesArray = new String[lines.size()];
+        lines.toArray(linesArray);
+        return linesArray;
+    }
+    public static int isFile() {
+        File file = new File("Motorcycle.txt");try {
+            if (file.createNewFile()) {
+                System.out.println("File Motorcycle.txt created");
+            } else {
+                FileReader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                System.out.println("File already exists.");
+                int lineCount = 0;
+                while (bufferedReader.readLine() != null) {
+                    lineCount++;
+                }
+
+                bufferedReader.close();
+                return lineCount;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // If an error occurs or the file doesn't exist, return 0
+        return 0;
+    }
+    public void addData() {
+        isFile();
+        try {
+            FileWriter myWriter = new FileWriter("Motorcycle.txt");
+            myWriter.write(this.toString());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void rentV(String vehi) {
+        GUI.moveVehicle("Motorcycle.txt","RMotorcycle.txt",vehi);
+    }
+
+    public static void returnV(String vehi) {
+        GUI.moveVehicle("RMotorcycle.txt","Motorcycle.txt",vehi);
     }
 
 }
