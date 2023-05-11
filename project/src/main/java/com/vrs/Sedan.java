2e342f9c5a35b52af0a0c0af107bf4ce66a4a3ef
HerobaneNair <srirambnair@gmail.com>
2023-05-11 12:16:16 -0400

    Update Sedan.java

--- project/src/main/java/com/vrs/Sedan.java
+++ project/src/main/java/com/vrs/Sedan.java
@@ -1,13 +1,89 @@
 package com.vrs;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Sedan extends Vehicle {
     public int numSeats;
-    public Sedan (String makeS, String modelS, int yearS, String colorS, String engineTypeS, int numSetasS)
+    public Sedan (String make, String model, int year, double mpg, String color, String engine, int seats)
     {
-           this.make = makeS;
-           this.model = modelS;
-           this.year = yearS;
-           this.color = colorS;
-           this.engineType = engineTypeS;
+        super(make, model, year, mpg, color, engine);
+        numSeats = seats;
    }
+    public String toString()
+    {
+        String out;
+        out = " Year: "+year+" "+color + " " + make + " " + model + ". MPG: "+ MPG + ", seats: "+numSeats;
+        return out;
+    }
+    public static String[] read() {
+        isFile();
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            // Create a new FileReader object for the file
+            FileReader reader = new FileReader("Sedan.txt");
+
+            // Create a new BufferedReader object to read from the FileReader
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            // Read each line of text from the file and add it to the ArrayList
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Close the BufferedReader object to release any resources it was using
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // Convert the ArrayList to a String array and return it
+        String[] linesArray = new String[lines.size()];
+        lines.toArray(linesArray);
+        return linesArray;
+    }
+    public static int isFile() {
+        File file = new File("Sedan.txt");try {
+            if (file.createNewFile()) {
+                System.out.println("File Sedan.txt created");
+            } else {
+                FileReader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                System.out.println("File already exists.");
+                int lineCount = 0;
+                while (bufferedReader.readLine() != null) {
+                    lineCount++;
+                }
+
+                bufferedReader.close();
+                return lineCount;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // If an error occurs or the file doesn't exist, return 0
+        return 0;
+    }
+    public void addData() {
+        isFile();
+        try {
+            FileWriter myWriter = new FileWriter("Sedan.txt");
+            myWriter.write(this.toString());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void rentV(String vehi) {
+        GUI.moveVehicle("Sedan.txt","RSedan.txt",vehi);
+    }
+
+    public static void returnV(String vehi) {
+        GUI.moveVehicle("RSedan.txt","Sedan.txt",vehi);
+    }
 }
